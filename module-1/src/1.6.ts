30714b906fb4fe06741a6ea23b7324f83c976b65
tuhinhossain123 <csetuhin55@gmail.com>
2025-01-28 09:38:15 +0600

    typescript method

--- module-1/src/1.6.ts
+++ module-1/src/1.6.ts
@@ -8,3 +8,13 @@ addNumber(2, 3);
 
 // arrow function
 const add = (num1: number, num2: number): number => num1 + num2;
+
+// method
+
+const poorUser = {
+  name: "Tuhin Hossain",
+  balance: 0,
+  addBalance(balance: number):string {
+    return ` my balance is: ${this.balance + balance}`;
+  },
+};
