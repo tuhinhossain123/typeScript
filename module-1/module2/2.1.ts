56663f15d23335d035b204fc8490c6eec6500c8e
tuhinhossain123 <csetuhin55@gmail.com>
2025-02-09 08:48:01 +0600

    type script type agertion

---
+++ module-1/module2/2.1.ts
@@ -0,0 +1,12 @@
+{
+  //
+
+  let anything: any;
+
+  anything = "My Name is Tuhin Hossain";
+  anything = 4807;
+
+  (anything as number).toFixed
+
+  //
+}
--- module-1/src/1.11.ts
+++ module-1/src/1.11.ts
@@ -1,9 +1,12 @@
 {
-    // ternaray operator
+  // ternaray operator
   const adult: number = 18;
-  if (adult >= 18) {
-    console.log("adult");
-  } else {
-    console.log("not adult");
-  }
+  //   if (adult >= 18) {
+  //     console.log("adult");
+  //   } else {
+  //     console.log("not adult");
+  //   }
+
+  const isAdult = adult >= 18 ? "adult" : "not adult";
+  console.log(isAdult);
 }
