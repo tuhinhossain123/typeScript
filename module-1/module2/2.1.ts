2b6fd2a83d1d791492ff16e3ddb8b81a495de82e
tuhinhossain123 <csetuhin55@gmail.com>
2025-02-09 08:58:12 +0600

    type agertion

--- module-1/module2/2.1.ts
+++ module-1/module2/2.1.ts
@@ -6,7 +6,18 @@
   anything = "My Name is Tuhin Hossain";
   anything = 4807;
 
-  (anything as number).toFixed
+  (anything as number).toFixed;
+
+  const kgToGm = (value: string | number): string | number | undefined => {
+    if (typeof value === "string") {
+      const convertedValue = parseFloat(value) * 1000;
+      return `The Converted Value is : ${convertedValue}`;
+    } else if (typeof value === "number") {
+      return value * 1000;
+    }
+  };
+
+  const check = kgToGm(32) as number;
 
   //
 }
