24252781ac21e012962e9ce8acf33745b5cd8024
tuhinhossain123 <csetuhin55@gmail.com>
2025-02-27 11:45:25 +0600

    typescript union type practice

--- module-1/src/1.10.ts
+++ module-1/src/1.10.ts
@@ -1,25 +1,7 @@
 {
-  //  union   type
+  //
+  type Money = "audi" | "BMW";
+  const myMonney: Money = "audi";
 
-  type Money = "Audi car" | "bike";
-  const amrTk: Money = "Audi car";
-
-  // intersection
-
-  type FrontEndDeveloper = {
-    skills: string[];
-    role1: "Frontend end developer";
-  };
-  type BackEndDeveloper = {
-    skills: string[];
-    role2: "Back end developer";
-  };
-
-  type FullStackDeveloper = FrontEndDeveloper & BackEndDeveloper;
-
-  const fullStackDeveloper: FullStackDeveloper = {
-    skills: ["js", "next js"],
-    role1: "Frontend end developer",
-    role2: "Back end developer",
-  };
+  //
 }
