c48e405abf124e4dedff60293008928d24ca26e3
tuhinhossain123 <csetuhin55@gmail.com>
2025-02-27 11:53:02 +0600

    tarnary oparators practice

--- module-1/src/1.11.ts
+++ module-1/src/1.11.ts
@@ -1,12 +1,8 @@
 {
+  //
   // ternaray operator
   const adult: number = 18;
-  //   if (adult >= 18) {
-  //     console.log("adult");
-  //   } else {
-  //     console.log("not adult");
-  //   }
-
   const isAdult = adult >= 18 ? "adult" : "not adult";
   console.log(isAdult);
+  //
 }
