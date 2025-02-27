170441b75c13dc70c99bd6a1af73a8704c630dfb
tuhinhossain123 <csetuhin55@gmail.com>
2025-02-27 11:51:50 +0600

    intersection practice

--- module-1/src/1.10.ts
+++ module-1/src/1.10.ts
@@ -1,7 +1,24 @@
 {
-  //
+  //union type
   type Money = "audi" | "BMW";
   const myMonney: Money = "audi";
 
+  // intersection type
+  type FrontEndDev = {
+    skills: string[];
+    role: "Front End Developer";
+  };
+  type BackEndDev = {
+    skills: string[];
+    role2: "Back End Developer";
+  };
+
+  type FullStackDev = FrontEndDev & BackEndDev;
+
+  const developer: FullStackDev = {
+    skills: ["HTML", "CSS", 'JS, "Typescript'],
+    role: "Front End Developer",
+    role2: "Back End Developer",
+  };
   //
 }
