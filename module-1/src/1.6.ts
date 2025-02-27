7c208f12e3e195825172ddf12f82365792ac0660
tuhinhossain123 <csetuhin55@gmail.com>
2025-02-27 10:21:25 +0600

    method practice

--- module-1/src/1.6.ts
+++ module-1/src/1.6.ts
@@ -1,20 +1,17 @@
-// normal function
+{
+  //
 
-function addNumber(num1: number, num2: number = 10): number {
-  return num1 + num2;
+  // method
+
+  const users = {
+    name: "Tuhin Hossain",
+    age: 23,
+    balance: 2020,
+
+    addBalance(balance: number): string {
+      return `user balance is  ${this.balance + balance}`;
+    },
+  };
+
+  //
 }
-
-addNumber(2, 3);
-
-// arrow function
-const add = (num1: number, num2: number): number => num1 + num2;
-
-// method
-
-const poorUser = {
-  name: "Tuhin Hossain",
-  balance: 0,
-  addBalance(balance: number):string {
-    return ` my balance is: ${this.balance + balance}`;
-  },
-};
