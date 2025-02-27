95a1b9a8108c78162e4c2fc1e485e040cd21f0be
tuhinhossain123 <csetuhin55@gmail.com>
2025-02-27 10:14:07 +0600

    non primitive data type practice

--- module-1/src/1.5.ts
+++ module-1/src/1.5.ts
@@ -1,10 +1,24 @@
-const user: {
-  readonly name: string;
-  age: number;
-  married: boolean;
-} = {
-  name: "Tuhin Hossain",
-  age: 23,
-  married: false,
-};
+{
+  //
+  // NON-Primitive data type
 
+  // array
+  let numbers: number[] = [1, 2, 3, 4];
+  numbers.push(10);
+
+  // object
+
+  const user: {
+    readonly name: "Tuhin Hossain";
+    age: number;
+    married?: boolean;
+  } = {
+    name: "Tuhin Hossain",
+    age: 22,
+  };
+
+  // function
+  const add = (num1: number, num2: number): number => num1 + num2;
+
+  //
+}
