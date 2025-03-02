bd9c367977754586c3b6f260ba6ed398849c009e
tuhinhossain123 <csetuhin55@gmail.com>
2025-03-02 16:05:11 +0600

    static oop

---
+++ module3/3.7.ts
@@ -0,0 +1,21 @@
+{
+  //static oop
+
+  class Counter {
+    static count: number = 0;
+    static increment() {
+      return (Counter.count = Counter.count + 1);
+    }
+    static decrement() {
+      return (Counter.count = Counter.count + 1);
+    }
+  }
+
+  console.log(Counter.increment());
+
+  console.log(Counter.increment());
+  console.log(Counter.increment());
+  console.log(Counter.increment());
+
+  //
+}
